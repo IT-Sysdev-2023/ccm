ff9838fd801c03ebd49c012c2607e2a3fafa0ba4
WebmasterSensei <webmasteraturservice@gmail.com>
2024-02-28 10:15:21 +0800

    stash

--- resources/js/Pages/Updates&Import/InstitutionalUpdateChecks.vue
+++ resources/js/Pages/Updates&Import/InstitutionalUpdateChecks.vue
@@ -1,7 +1,7 @@
 <script setup>
 import TreasuryLayout from '@/Layouts/TreasuryLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import { SmileOutlined, QuestionOutlined, UploadOutlined } from '@ant-design/icons-vue';
+import { SmileOutlined, QuestionOutlined, UpSquareOutlined, ImportOutlined, UploadOutlined } from '@ant-design/icons-vue';
 const placement = 'top';
 
 const tabPosition = 'right';
@@ -17,7 +17,122 @@ const tabPosition = 'right';
         <div class="py-5">
 
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition" animated>
+
+                <a-row :gutter="[16, 16]">
+                    <a-col :span="6">
+                        <a-card style="height: 100%;">
+                            <a-button style="width: 100%;" class="mb-5" @click="showImportInstutional">
+                                <ImportOutlined />
+                                Import Institutional Checks
+                            </a-button>
+                            <a-button style="width: 100%;" @click="showUpdateddatabase">
+                                <UpSquareOutlined />
+                                Update Atp Database
+                            </a-button>
+                        </a-card>
+                    </a-col>
+                    <a-col :span="18">
+                        <a-card>
+                            <div class="loader mt-0" v-if="showImport">
+                                <div class="book-wrapper">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 126 75" class="book">
+                                        <rect stroke-width="5" stroke="#e05452" rx="7.5" height="70" width="121" y="2.5"
+                                            x="2.5"></rect>
+                                        <line stroke-width="5" stroke="#e05452" y2="75" x2="63.5" x1="63.5"></line>
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M25 20H50"></path>
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M101 20H76">
+                                        </path>
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M16 30L50 30">
+                                        </path>
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M110 30L76 30">
+                                        </path>
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff74" viewBox="0 0 65 75"
+                                        class="book-page">
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M40 20H15"></path>
+                                        <path stroke-linecap="round" stroke-width="4" stroke="#c18949" d="M49 30L15 30">
+                                        </path>
+                                        <path stroke-width="5" stroke="#e05452"
+                                            d="M2.5 2.5H55C59.1421 2.5 62.5 5.85786 62.5 10V65C62.5 69.1421 59.1421 72.5 55 72.5H2.5V2.5Z">
+                                        </path>
+                                    </svg>
+
+                                </div>
+                                <a-result status="" title="Hi There!"
+                                    sub-title="Click  the start button, to import the institutional text-file.">
+                                    <template #extra>
+                                        <a-upload v-model:file-list="fileList" name="file"
+                                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76" :headers="headers"
+                                            @change="handleChange">
+                                            <a-button style="background: #4791d6; color: white !important;">
+                                                <UploadOutlined />
+                                                Start importing
+                                            </a-button>
+                                        </a-upload>
+                                    </template>
+                                </a-result>
+                            </div>
+                            <div class="loader mt-0" v-if="showUpdate">
+                                <div class="book-wrapper">
+
+                                    <div class="gearbox">
+                                        <div class="overlay"></div>
+                                        <div class="gear one">
+                                            <div class="gear-inner">
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                            </div>
+                                        </div>
+                                        <div class="gear two">
+                                            <div class="gear-inner">
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                            </div>
+                                        </div>
+                                        <div class="gear three">
+                                            <div class="gear-inner">
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                            </div>
+                                        </div>
+                                        <div class="gear four large">
+                                            <div class="gear-inner">
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                                <div class="bar"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <a-result status="" title="Hi There!"
+                                    sub-title="Click  the start button, to update the atp database.">
+                                    <template #extra>
+                                        <a-upload v-model:file-list="fileList" name="file"
+                                            action="https://www.mocky.io/v2/5cc8019d300000980a055e76" :headers="headers"
+                                            @change="handleChange">
+                                            <a-button style="background: #4791d6; color: white !important;">
+                                                <UploadOutlined />
+                                                start to update
+                                            </a-button>
+                                        </a-upload>
+                                    </template>
+                                </a-result>
+                            </div>
+                            <div class="loader mt-0">
+
+                            </div>
+                        </a-card>
+                    </a-col>
+                </a-row>
+                <!-- <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition" animated>
                     <a-tab-pane key="1" tab="Import Institutional Checks">
                         <div class="text-xl">
                             <SmileOutlined /> Import Institutional Check
@@ -79,7 +194,7 @@ const tabPosition = 'right';
                             </template>
                         </a-result>
                     </a-tab-pane>
-                </a-tabs>
+                </a-tabs> -->
 
             </div>
         </div>
@@ -130,6 +245,8 @@ export default {
     data() {
         return {
             jokes: '',
+            showUpdate: false,
+            showImport: false,
         }
     },
     methods: {
@@ -143,6 +260,14 @@ export default {
                 console.error("Error fetching joke:", error);
             }
         },
+        showImportInstutional() {
+            this.showImport = true;
+            this.showUpdate = false;
+        },
+        showUpdateddatabase() {
+            this.showImport = false;
+            this.showUpdate = true;
+        }
     },
     mounted() {
         this.getRandomJoke();
@@ -196,5 +321,178 @@ export default {
     100% {
         transform: rotateY(180deg) skewY(0deg);
     }
+}
+
+@keyframes clockwise {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+@keyframes counter-clockwise {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(-360deg);
+    }
+}
+
+.gearbox {
+    background: #111;
+    height: 170px;
+    width: 280px;
+    position: relative;
+    border: none;
+    overflow: hidden;
+    border-radius: 6px;
+    box-shadow: 0px 0px 0px 1px rgba(255, 255, 255, 0.1);
+}
+
+.gearbox .overlay {
+    border-radius: 6px;
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    box-shadow: inset 0px 0px 20px black;
+    transition: background 0.2s;
+}
+
+.gearbox .overlay {
+    background: transparent;
+}
+
+.gear {
+    position: absolute;
+    height: 60px;
+    width: 60px;
+    box-shadow: 0px -1px 0px 0px #888888, 0px 1px 0px 0px black;
+    border-radius: 30px;
+}
+
+.gear.large {
+    height: 120px;
+    width: 120px;
+    border-radius: 60px;
+}
+
+.gear.large:after {
+    height: 96px;
+    width: 96px;
+    border-radius: 48px;
+    margin-left: -48px;
+    margin-top: -48px;
+}
+
+.gear.one {
+    top: 12px;
+    left: 10px;
+}
+
+.gear.two {
+    top: 61px;
+    left: 60px;
+}
+
+.gear.three {
+    top: 110px;
+    left: 10px;
+}
+
+.gear.four {
+    top: 13px;
+    left: 128px;
+}
+
+.gear:after {
+    content: "";
+    position: absolute;
+    height: 36px;
+    width: 36px;
+    border-radius: 36px;
+    background: #111;
+    top: 50%;
+    left: 50%;
+    margin-left: -18px;
+    margin-top: -18px;
+    z-index: 3;
+    box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.1), inset 0px 0px 10px rgba(0, 0, 0, 0.1), inset 0px 2px 0px 0px #090909, inset 0px -1px 0px 0px #888888;
+}
+
+.gear-inner {
+    position: relative;
+    height: 100%;
+    width: 100%;
+    background: #555;
+    border-radius: 30px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+.large .gear-inner {
+    border-radius: 60px;
+}
+
+.gear.one .gear-inner {
+    animation: counter-clockwise 3s infinite linear;
+}
+
+.gear.two .gear-inner {
+    animation: clockwise 3s infinite linear;
+}
+
+.gear.three .gear-inner {
+    animation: counter-clockwise 3s infinite linear;
+}
+
+.gear.four .gear-inner {
+    animation: counter-clockwise 6s infinite linear;
+}
+
+.gear-inner .bar {
+    background: #555;
+    height: 16px;
+    width: 76px;
+    position: absolute;
+    left: 50%;
+    margin-left: -38px;
+    top: 50%;
+    margin-top: -8px;
+    border-radius: 2px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+    border-right: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+.large .gear-inner .bar {
+    margin-left: -68px;
+    width: 136px;
+}
+
+.gear-inner .bar:nth-child(2) {
+    transform: rotate(60deg);
+}
+
+.gear-inner .bar:nth-child(3) {
+    transform: rotate(120deg);
+}
+
+.gear-inner .bar:nth-child(4) {
+    transform: rotate(90deg);
+}
+
+.gear-inner .bar:nth-child(5) {
+    transform: rotate(30deg);
+}
+
+.gear-inner .bar:nth-child(6) {
+    transform: rotate(150deg);
 }
 </style>
