92fe6e25ba61f394c80b57a7ae905badac06a5e0
WebmasterSensei <webmasteraturservice@gmail.com>
2024-03-06 10:51:20 +0800

    another commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
 
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
