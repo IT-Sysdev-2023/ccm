c04e321f3ae623da758680d14404a81d8fccfe8b
WebmasterSensei <webmasteraturservice@gmail.com>
2024-04-15 16:33:40 +0800

    another commitd'

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,8 +20,12 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            if (Auth::guard($guard)->check() && Auth::user()->usertype_id == 1) {
+                return redirect(RouteServiceProvider::ADMIN_HOME);
+            }elseif (Auth::guard($guard)->check() && Auth::user()->usertype_id == 9){
+                return redirect(RouteServiceProvider::TREASURY_HOME);
+            }else{
+                return redirect(RouteServiceProvider::ACCOUNTING_HOME);
             }
         }
 
