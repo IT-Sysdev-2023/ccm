a3c9da1ab7e19a45047183b434a22ced6a008ed1
WebmasterSensei <webmasteraturservice@gmail.com>
2024-04-18 10:58:42 +0800

    another commits

--- resources/js/Pages/AdminDashboard.vue
+++ resources/js/Pages/AdminDashboard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import AdminLayout from '@/Layouts/AdminLayout.vue';
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 </script>
 
 <template>
@@ -7,12 +7,72 @@ import AdminLayout from '@/Layouts/AdminLayout.vue';
 
     <AdminLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
         </template>
 
-        <div class="py-4">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            </div>
+        <div>
+            <a-row :gutter="[16, 16]">
+                <a-col :span="12">
+                    <div id="chart">
+                        <apexchart
+                            type="polarArea"
+                            width="380"
+                            :options="chartOptions"
+                            :series="series"
+                        ></apexchart>
+                    </div>
+                </a-col>
+                <a-col :span="12"> </a-col>
+            </a-row>
         </div>
     </AdminLayout>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            series: [42, 47, 52, 58, 65],
+            chartOptions: {
+                chart: {
+                    width: 380,
+                    type: "polarArea",
+                },
+                labels: ["Rose A", "Rose B", "Rose C", "Rose D", "Rose E"],
+                fill: {
+                    opacity: 1,
+                },
+                stroke: {
+                    width: 1,
+                    colors: undefined,
+                },
+                yaxis: {
+                    show: false,
+                },
+                legend: {
+                    position: "bottom",
+                },
+                plotOptions: {
+                    polarArea: {
+                        rings: {
+                            strokeWidth: 0,
+                        },
+                        spokes: {
+                            strokeWidth: 0,
+                        },
+                    },
+                },
+                theme: {
+                    monochrome: {
+                        enabled: true,
+                        shadeTo: "light",
+                        shadeIntensity: 0.6,
+                    },
+                },
+            },
+        };
+    },
+};
+</script>
