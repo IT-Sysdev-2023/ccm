36b6149556fd4940cbacc9d0ae3bbe86529e17c3
WebmasterSensei <webmasteraturservice@gmail.com>
2024-04-18 14:28:48 +0800

    another commits

--- resources/js/Pages/AdminDashboard.vue
+++ resources/js/Pages/AdminDashboard.vue
@@ -55,3 +55,111 @@ import dayjs from "dayjs";
         </div>
     </AdminLayout>
 </template>
+<script>
+export default {
+    props: {
+        pmCounts: Object,
+        icm: Object,
+        asc: Object,
+        pmWeekly: Object,
+        icmWeekly: Object,
+        ascMainWeekly: Object,
+    },
+    data() {
+        return {
+            series: [this.pmCounts, this.icm, this.asc],
+            chartOptions: {
+                chart: {
+                    width: 380,
+                    type: "polarArea",
+                },
+                labels: ["PLAZA MARCELA", "ISLAND CITY MALL", "ASC-MAIN"],
+                fill: {
+                    opacity: 1,
+                },
+                stroke: {
+                    colors: ['#fff']
+                },
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
+                    }
+                }]
+            },
+            series2: [{
+                name: 'PLAZA MARCELA',
+                type: 'column',
+                data: this.pmWeekly.map(item => item.y)
+            }, {
+                name: 'ISLAND CITY MALL',
+                type: 'area',
+                data: this.icmWeekly.map(item => item.y)
+            }, {
+                name: 'ASC-MAIN',
+                type: 'line',
+                data: this.ascMainWeekly.map(item => item.y)
+            }],
+            chartOptions2: {
+                chart: {
+                    height: 350,
+                    type: 'line',
+                    stacked: false,
+                },
+                stroke: {
+                    width: [0, 2, 5],
+                    curve: 'smooth'
+                },
+                plotOptions: {
+                    bar: {
+                        columnWidth: '50%'
+                    }
+                },
+
+                fill: {
+                    opacity: [0.85, 0.25, 1],
+                    gradient: {
+                        inverseColors: false,
+                        shade: 'light',
+                        type: "vertical",
+                        opacityFrom: 0.85,
+                        opacityTo: 0.55,
+                        stops: [0, 100, 100, 100]
+                    }
+                },
+                labels: this.pmWeekly.map(item => dayjs(item.x).format('MMM-DD')),
+                markers: {
+                    size: 0
+                },
+                // xaxis: {
+                //     type: 'datetime'
+                // },
+                yaxis: {
+                    title: {
+                        text: 'Weekly Cheques',
+                    },
+                    min: 0
+                },
+                tooltip: {
+                    shared: true,
+                    intersect: false,
+                    y: {
+                        formatter: function (y) {
+                            if (typeof y !== "undefined") {
+                                return y.toFixed(0) + " Cheques";
+                            }
+                            return y;
+
+                        }
+                    }
+                }
+            },
+        };
+    },
+};
+</script>
